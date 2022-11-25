f5caa7496d4e4032be87ed0a0e91cb989537ea1c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-25 17:06:42 -0300

    7 - Requisito

---
+++ src/components/Missions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">Missions</div>
+    );
+  }
+}
+export default Missions;

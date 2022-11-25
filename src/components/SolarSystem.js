d29a3500d21a8c993520d72abb9f92cec698cc6a
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-25 15:24:15 -0300

    2 - Requisito

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>qualquer coisa</h2>
+      </div>
+    );
+  }
+}
+export default SolarSystem;
